f1b9712c91e00121c6fc284256ed220f6d876f4d
Swebb <duckliningdon@protonmail.com>
2023-03-01 22:36:27 -0600

    Initial Commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+echo "Switching to branch master"
+git checkout master
+
+echo "Building app..."
+npm run Building
+
+echo "Deploying files to server..."
+scp -r build/* joker@192.168.0.21:/var/www/samuelswedberg.com
+
+echo "Done!"
